23a8196ebee0217c9644ea7f88adfcc2e2b01a30
Ramana K <ramanak003@gmail.com>
2026-03-11 16:56:59 +0530

    fix: correct basePath for GitHub deployment

--- next.config.ts
+++ next.config.ts
@@ -2,13 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: process.env.GITHUB_ACTIONS === 'true' ? 'export' : undefined,
+  basePath: process.env.GITHUB_ACTIONS === 'true' ? '/Rex-webiste' : '',
+  trailingSlash: true,
   images: {
     unoptimized: true,
-  },
-  devIndicators: false,
-  turbopack: {
-    root: '.',
   },
 };
 
 export default nextConfig;
+
+
+
